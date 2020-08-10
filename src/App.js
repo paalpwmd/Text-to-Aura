98537dbd3d93789c9aff15ad3fb5777c7e238189
paalwilliams <paalpwmd>
2020-08-09 22:30:46 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import TextInput from './components/TextInput';
 import Swatch from './components/Swatch';
 import './App.css';
 
@@ -7,6 +6,7 @@ function App() {
   const [colors, setColors] = useState([]);
   const [hex, setHex] = useState([]);
   const [text, setText] = useState();
+
   useEffect(() => {
     if (hex.length === 3) {
       let newColor = hex.join('');
@@ -33,19 +33,39 @@ function App() {
   return (
     <div className='App'>
       <div id='titlecontainer'>
-        <h1 className='title'>What is</h1>
-        <h1 className='title'>Your Name's Aura</h1>
-        <h1 className='title'>?</h1>
+        <h1 className='title'>
+          <span id='cutout'>WORD</span>
+          <span id='gradient'>AURA</span>
+        </h1>
       </div>
-      <h2 id='directions'>Enter Text Here</h2>
-      <TextInput func={handleChange} />
+      <h2 id='directions'>
+        Enter your text here and witness as the letters are turned, alchemy like
+        into a beautiful color palette
+      </h2>
+      <div className='inputcontainer box'>
+        <input
+          type='text'
+          name=''
+          id='auraInput'
+          onChange={handleChange}
+          value={text}
+          placeholder='Enter Text Here'
+        />
+      </div>
       <div id='swatchPalette'>
         {colors.map((color) => {
           return <Swatch color={color} />;
         })}
       </div>
+      <div id='codecontainer'>
+        <code>cssGradientGeneration = "comingSoon";</code>
+      </div>
       <p id='colophon'>
-        Site by Paal Williams | Design by{' '}
+        Site by{' '}
+        <a href='https://paaa.al' target='_blank'>
+          Paal Williams
+        </a>{' '}
+        | Design by
         <a href='https://jakehansondesign.com/' target='_blank'>
           Jake Hanson
         </a>
